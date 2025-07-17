c3b57b3f6f2eec0600c9cfa8f2dc227aa4c1bb1a
Buenaventura123 <202310536@fit.edu.ph>
2025-07-17 10:59:27 +0800

    feat: created login authentication

---
+++ utils/auth.util.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+class Auth
+{
+    public static function init(): void
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public static function login(string $email, string $password): bool
+    {
+        self::init();
+        require_once UTILS_PATH . '/dbConnection.util.php';
+        $pdo = getDatabaseConnection();
+
+        $stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email LIMIT 1');
+        $stmt->execute(['email' => $email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user'] = $user;
+            return true;
+        }
+        return false;
+    }
+
+}
