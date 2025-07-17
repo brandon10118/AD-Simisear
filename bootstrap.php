e115496355ffba74faf0565b705f236c85c1b75f
JethroWang24 <202311220@fit.edu.ph>
2025-07-17 12:07:37 +0800

    feat: initialize application paths and load environment variables in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,20 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
+define('VENDOR_PATH', realpath(BASE_PATH . "/vendor"));
+define('COMPONENT_PATH', realpath(BASE_PATH . "/components"));
+define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('STATICDATA_PATH', realpath(BASE_PATH . "/staticData"));
+define('LAYOUT_PATH', realpath(BASE_PATH . "/layout"));
+define('ASSETS_PATH', realpath(BASE_PATH . "/assets"));
 
 chdir(BASE_PATH);
+
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once UTILS_PATH . '/auth.util.php';
+Auth::init();
+
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
+
+?>
