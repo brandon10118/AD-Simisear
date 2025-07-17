e691154cc51626f3be7605d768204becc103f350
Buenaventura123 <202310536@fit.edu.ph>
2025-07-17 10:43:56 +0800

    feat: mineral data created

---
+++ staticDatas/mineral.staticData.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    [
+        'name' => 'Amethyst',
+        'origin' => 'Greece',
+        'type' => 'Gemstone',
+        'description' => 'A violet variety of quartz prized in ancient Greece.',
+        'image' => 'amethyst.jpg',
+        'price' => 100,
+        'stock' => 10
+    ],
+];
